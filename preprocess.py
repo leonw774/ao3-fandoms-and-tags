f344039a5a6ba73552c6fdd2a0456cc3c2883e0a
leonw774 <leonw774@gmail.com>
2019-06-05 00:41:27 +0800

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -20,15 +20,13 @@ key_word = [
 "hurt/comfort",
 "time travel",
 ]
-'''
+
 synonyms = {
+"au" : "alternate universe",
 "a/b/o" : "alpha/beta/omega dynamics",
 "abo" : "alpha/beta/omega dynamics",
-"alpha" : "alpha/beta/omega dynamics",
-"beta" : "alpha/beta/omega dynamics",
-"omega" : "alpha/beta/omega dynamics",
 }
-'''
+
 all_tags_count = {}
 tags_fandoms_counter = {}
 
@@ -61,14 +59,14 @@ for df in dfs :
             tags_set.add(tag_add)
         for tag_rm in tags_to_remove :
             tags_set.remove(tag_rm)
-        '''
+        
         # replace tags with synonyms
         for nym in synonyms.keys() :
             if nym in tags_set :
                 tags_set.remove(nym)
                 if not synonyms[nym] in tags_set :
                     tags_set.add(synonyms[nym])
-        '''
+        
         # save preprocessed tags
         df.at[i, "tags"] = str(tags_set)
         
@@ -88,7 +86,7 @@ for df in dfs :
 
 # decide min_remove_count
 counts = [all_tags_count[key] for key in all_tags_count]
-min_remove_count = int(np.mean(counts) + 1)
+min_remove_count = int(np.quantile(counts, 0.9))
 
 print("before tags count:", len(counts))
 print("max count:", max(counts))
@@ -102,8 +100,8 @@ for df in dfs :
         for tag in tags_set :
             if not tag in tags_to_remove :
                 # delete tags that only appear in ONE fandom
-                if len(tags_fandoms_counter[tag]) <= 1 :
-                    tags_to_remove.append(tag)
+                #if len(tags_fandoms_counter[tag]) <= 1 :
+                #    tags_to_remove.append(tag)
                 # delete tags that appear less than min
                 if all_tags_count[tag] < min_remove_count :
                     tags_to_remove.append(tag)
